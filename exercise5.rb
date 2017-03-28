b8c002f887a221a6edb7bae1c5819d73fdedbad0
jpileto <jp.ileto@icloud.com>
2017-03-28 16:32:27 -0400

    Done with exercise 6 temp conversion

---
+++ exercise5.rb
@@ -0,0 +1,9 @@
+def greet_backwards(name)
+  puts "Hello, #{name.reverse}#{name.reverse}! Welcome home."
+end
+
+greet_backwards("Amanda")
+greet_backwards("Bob")
+greet_backwards("Shirley")
+greet_backwards("Sue")
+greet_backwards("Andy")
